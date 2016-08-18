5a9afecf8d2023c1df887b3d1c1d63b21032d4e8
buddhikajay <buddhika.12@cse.mrt.ac.lk>
2016-08-19 01:24:15 +0530

    Closed a Bracket in Fos:UserBundle:checkEmail.html.twig

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+chmod 777 -R /sites/edulink/edulinkELearning/web/
+
+composer update && \
+php bin/console assetic:dump --env=prod --no-debug && \
+php bin/console doctrine:schema:update  --force && \
+php bin/console cache:clear --env=prod --no-debug && \
+chmod -R 777 var/cache var/logs && \
+php bin/console assets:install
